688744b8296e733170b47c168b32f1512c820033
SREERAG K <csreeragk@gmail.com>
2017-09-24 21:22:08 +0530

    updated the item list div
    item
    description and images added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipeData: RecipeModel[]= [
+    new RecipeModel('burger',
+              'burger is the american foof filled awith all sorts of vegetables it seems to berich in proteins',
+               'https://www.franchiseindia.com/uploads/content/ri/art/fast-food-586d2c1ff8.jpg?d=2'),
+               new RecipeModel('PIZZA',
+               'PIZZA is the american foof filled awith all sorts of vegetables it seems to berich in proteins',
+                'https://cdn.modpizza.com/wp-content/uploads/2016/11/mod-pizza-calexico-default-e1479167492741.png') 
+               ];
   constructor() { }
 
   ngOnInit() {
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RecipeComponent  {
+  constructor() {
   }
-
 }
